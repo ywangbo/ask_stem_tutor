b63d62d447937ea1fe14dbe3d60fa9c1d50b1326
ywangbo <ywangbo22@gmail.com>
2024-01-29 17:44:53 -0500

    Create a website for tutoring in STEM subjects.

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+import askAI
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = ""
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_response', methods=['POST'])
+def get_response():
+    user_question = request.form['user_question']
+    response_paragraph = askAI.write_a_function(user_question)
+    return render_template('index.html', response_paragraph=response_paragraph)
